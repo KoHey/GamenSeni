9687b29b336e06ce4ef642679d0210df7f74dfc9
k.sugawara <k.sugawara@and-ex.co.jp>
2017-10-03 12:54:55 +0900

    2nd Kotlin Android Extensions使う
    便利やね

--- app/src/main/java/com/example/ksugawara/gamenseni/MainActivity.kt
+++ app/src/main/java/com/example/ksugawara/gamenseni/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private val editText: EditText by lazy {
@@ -17,9 +19,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        button2.setOnClickListener{
+            textView.text = "waaaaaaaaaaaaa"
+        }
+
+        textView.text = "aiueo"//kotlin-android-extensionsすごい
         //editText = findViewById(R.id.editText) as EditText
         val button = findViewById(R.id.button) as Button
         button.setOnClickListener{onClick()}
+        button2.setOnClickListener{ changeText(textView,"waaaaaaaaao") }
     }
     fun onClick() {
 //        val text = editText.text?.toString()
@@ -30,4 +38,8 @@ class MainActivity : AppCompatActivity() {
         intent.putExtra("DATA", editText.text.toString())
         startActivity(intent)
     }
+    fun changeText(tV: TextView, text: String) {
+        tV.text = text
+    }
+
 }
